81603ec99c3e352fd8b771fbef81b734aec5d524
Hiyaro <nawfal.elkhaznagi@gmail.com>
2022-04-30 01:26:15 +0200

    fin des exercices

--- exo.array/exo_math/public/js/main.js
+++ exo.array/exo_math/public/js/main.js
@@ -12,6 +12,19 @@ console.log(Math.floor(nombre));
 console.log(Math.ceil(nombre));
 
 // vas retirer les decimales
-console.log(Math.trunc(nombre))
+console.log(Math.trunc(nombre));
 
-console.log(Math.random() * 10)
+
+console.log(Math.random());
+
+console.log(Math.random() * 100);
+
+console.log(8 ** 2);
+
+console.log(Math.sqrt(9))
+
+console.log(Math.abs(-1));
+
+console.log(Math.max(-2, 1000, 8, 57));
+
+console.log(Math.min(-2, 1000, 8, 57));
