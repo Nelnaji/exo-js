9c53906c9a9e9bfe8cd4e88f9a6e121657f95587
Hiyaro <nawfal.elkhaznagi@gmail.com>
2022-04-29 11:06:03 +0200

    second salve of exercices

---
+++ exo.array/exo1/public/js/index.js
@@ -0,0 +1,20 @@
+let array = ["coding11", "coding12", "coding13"];
+
+console.log(array);
+console.log(array.toString());
+
+console.log(array.length);
+// Ont peux utiliser cette methode manuelle pour afficher chaque array separemment
+console.log(array[0]);
+console.log(array[1]);
+console.log(array[2]);
+
+
+
+// ont peux utiliser une loop pour les afficher un par un
+
+
+console.log('------------ loop method ------------')
+for (i = 0; i < array.length; i++) {
+    console.log(array[i])
+}
---
+++ exo.array/exo2/public/js/main.js
@@ -0,0 +1,3 @@
+const array = ["coding 11", "coding 12", "coding 13"];
+
+console.log(array.toString());
