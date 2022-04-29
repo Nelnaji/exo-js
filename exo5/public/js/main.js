9638fe8170a22ebc01632d53cf0d66b0fe73ecf7
Hiyaro <nawfal.elkhaznagi@gmail.com>
2022-04-29 10:05:55 +0200

    first commit

--- exo5/public/js/main.js
+++ exo5/public/js/main.js
@@ -5,4 +5,15 @@ console.log(phrase.length);
 let phrase2 = phrase.slice(0, -1);
 console.log(phrase2.length);
 
-console.log(phrase2[]);
+
+let phrase3 = "tout le monde";
+
+console.log(phrase3);
+
+let phrase4 = phrase3.replace("", "Bonjour ");
+
+console.log(phrase4);
+
+let phrase5 = phrase4.replace("tout le monde", "");
+
+console.log(phrase5);
